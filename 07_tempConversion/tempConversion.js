8a99394f016113524c6e988441784b291db0de10
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-13 09:58:21 +1000

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  celcius = (num-32)*5/9;
+  return round((celcius*10)/10, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  fahrenheit = (((9/5)*num)+32);
+  return round((fahrenheit*10)/10, 1);
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
