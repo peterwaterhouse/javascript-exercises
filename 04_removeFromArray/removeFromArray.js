b5a578a5c8db59ea5de0e4b95fd19c3462154f8e
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-13 09:23:07 +1000

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...nums) {
+    for(i = 0; i < nums.length; i++) {
+        for(j = 0; j < myArray.length; j++) {
+            if (nums[i] === myArray[j]) {
+                myArray.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
