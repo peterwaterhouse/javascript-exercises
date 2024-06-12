453338699936e5fc4e8fbbcebac9c0be76780f85
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-13 09:02:42 +1000

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newString = "";
+    for(i = string.length; i >= 0; i--) {
+        newString = newString + string.charAt(i);
+    }
+    return newString;
 };
 
 // Do not edit below this line
