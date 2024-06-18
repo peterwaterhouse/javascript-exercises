b544d70077a2b9352a6071c658a350b844851aec
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-18 16:31:44 +1000

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    stripped = s.replace(/[^A-Za-z0-9]/g, '');
+    lowerCase = stripped.toLowerCase();
+    rev = lowerCase.split("").reverse().join("");
+    if (lowerCase == rev) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
