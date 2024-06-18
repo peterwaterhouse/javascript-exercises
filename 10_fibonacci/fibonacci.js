b543d7d3915ea75fd57066c732be1fa273794b15
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-18 16:40:52 +1000

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    if (arg < 0) {
+        return "OOPS";
+    }
+    if (arg == 1 || arg == 2) {
+        return 1;
+    } 
+    let num1 = 1;
+    let num2 = 1;
+    let num3 = 0;
+    for (i = 0; i < arg-2; i++) {
+        num3 = num2 + num1;
+        num1 = num2;
+        num2 = num3;
+    }
+    return num3;
 };
 
 // Do not edit below this line
